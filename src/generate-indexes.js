d5cd7d1b6c29bfcf217dd5680066ce59dc25b75b
Richard Tribe <Richard.Tribe@bbc.co.uk>
2019-10-17 10:39:02 +0100

    Remove plusification

--- src/generate-indexes.js
+++ src/generate-indexes.js
@@ -12,7 +12,7 @@ const generateIndex = (htmlFilePaths, subDirPaths, contentTitle) => {
   }))
 
   const fileLinks = htmlFilePaths.map(filePath => {
-    const fileName = path.basename(filePath).replace(/\+/g, 'plus')
+    const fileName = path.basename(filePath)
     const encodedFileName = encodeURIComponent(fileName)
     const encodedFilePath = `${path.dirname(fileName)}/${encodedFileName}`
 
