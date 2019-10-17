64c09ef551f6af6cfccf02742520f88622ddb3d7
Richard Tribe <Richard.Tribe@bbc.co.uk>
2019-10-17 17:23:30 +0100

    Remove unnecessary escape character: \/.

--- src/generate-transform-input.js
+++ src/generate-transform-input.js
@@ -2,7 +2,7 @@ const readdir = require('recursive-readdir')
 const fs = require('fs')
 
 const generateTransformInput = (dir) => {
-  const matchFirstDir = /^[^\/]*\// // finds the first dir, i.e will match 'www/' from 'www/foo'
+  const matchFirstDir = /^[^/]*\// // finds the first dir, i.e will match 'www/' from 'www/foo'
 
   return readdir(dir).then(files => {
     return files.map(file => {
