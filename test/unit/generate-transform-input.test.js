9598a71bb7069c8b8723d0a2b5edf78360e33d15
Adam Walters <adam.walters@bbc.co.uk>
2019-10-28 14:43:30 +0000

    Add extra test case for dirs passed in with no /

--- test/unit/generate-transform-input.test.js
+++ test/unit/generate-transform-input.test.js
@@ -5,11 +5,11 @@ const fs = require('fs')
 jest.mock('recursive-readdir')
 jest.mock('fs')
 
-readdir.mockImplementation(() => Promise.resolve(['/someDirectory/some/path/tofile.md', '/someDirectory/another/path/tofile.md']))
+readdir.mockImplementation(() => Promise.resolve(['someDirectory/some/path/tofile.md', 'someDirectory/another/path/tofile.md']))
 fs.readFileSync.mockImplementation(() => 'some text in a file')
 
 describe('Generate transform input', () => {
-  it('creates a correctly strcutured array of file objects from a directory', async () => {
+  it('creates a correctly structured array of file objects from a directory ending in a slash', async () => {
     const expected = [
       {
         relativePath: 'some/path/tofile.md',
@@ -21,7 +21,24 @@ describe('Generate transform input', () => {
       }
     ]
 
-    const actual = await generateTransformInput('/someDirectory/')
+    const actual = await generateTransformInput('someDirectory/')
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('creates a correctly structured array of file objects from a directory that does not end in a slash', async () => {
+    const expected = [
+      {
+        relativePath: 'some/path/tofile.md',
+        raw: 'some text in a file'
+      },
+      {
+        relativePath: 'another/path/tofile.md',
+        raw: 'some text in a file'
+      }
+    ]
+
+    const actual = await generateTransformInput('someDirectory')
 
     expect(actual).toEqual(expected)
   })
