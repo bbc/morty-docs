328ca036ec8133eb641d0b293c219565d618340c
Pete Rwatschew <peter.rwatschew.p123@gmail.com>
2019-10-29 14:33:43 +0000

    fix linting

--- src/helpers/filter-files-in-dir.js
+++ src/helpers/filter-files-in-dir.js
@@ -1,4 +1,4 @@
-const dateRegex = /\d{4}-\d{2}-\d{2}/ // 2019-10-29
+const dateRegex = /\d{4}-\d{2}-\d{2}/ // i.e. 2019-10-29
 
 function sortByDate (a, b) {
   return a.match(dateRegex) && b.match(dateRegex) ? b.localeCompare(a) : a.localeCompare(b)
@@ -6,7 +6,7 @@ function sortByDate (a, b) {
 
 function filterFilesInDir (filePaths, directory) {
   return filePaths.filter(filePath => {
-    return directory // directory = '' is falsy 
+    return directory // directory = '' is falsy
       ? filePath.startsWith(directory)
       : !filePath.includes('/') // returns true for files in root directory
   })
