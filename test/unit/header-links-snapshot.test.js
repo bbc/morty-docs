1a7c3da3f6fd9957a4d7eb4dc19d0bb9896c025a
Adam Walters <adam.walters@bbc.co.uk>
2020-08-20 13:55:01 +0100

    Fix linting

--- test/unit/header-links-snapshot.test.js
+++ test/unit/header-links-snapshot.test.js
@@ -2,11 +2,10 @@ const React = require('react')
 const Header = require('../../src/page-renderers/Components/Header')
 const renderer = require('react-test-renderer')
 
-
 describe('The header is rendered with the correct breadcrumb links ', () => {
   it('When a basepath is passed through', () => {
     const component = renderer.create(
-      <Header relPath={'folder/nestedFolder/someFile.html'} basePath={'morty-docs/some-repo'}></Header>
+      <Header relPath={'folder/nestedFolder/someFile.html'} basePath={'morty-docs/some-repo'} />
     )
 
     let tree = component.toJSON()
@@ -15,7 +14,7 @@ describe('The header is rendered with the correct breadcrumb links ', () => {
 
   it('When a basepath is not passed through', () => {
     const component = renderer.create(
-      <Header relPath={'folder/nestedFolder/someFile.html'} basePath={''}></Header>
+      <Header relPath={'folder/nestedFolder/someFile.html'} basePath={''} />
     )
 
     let tree = component.toJSON()
