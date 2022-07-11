189d7aa8a83f0a82dcd1eeff1b80ececbf577e01
Kayle Patton <kayle.patton@bbc.co.uk>
2022-07-11 11:26:31 +0100

    lint run.js file

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ const { transform, generateTransformInput } = require('./build')
 
 const path = require('path')
 
-const directoryToConvert = process.env.mortyPath|| 'default-md-files'
+const directoryToConvert = process.env.mortyPath || 'default-md-files'
 const resolvedPath = path.resolve(directoryToConvert)
 
 const mortyDocs = async () => {
