3e32473562fe496494c93a6ded7866944e7cc953
Kayle Patton <kayle.patton@bbc.co.uk>
2022-07-18 15:56:00 +0100

    lint date component

--- src/page-renderers/Components/PrettyDate.jsx
+++ src/page-renderers/Components/PrettyDate.jsx
@@ -1,11 +1,11 @@
 const React = require('react')
 
-const nth = function(d) {
+const nth = function (d) {
   if (d > 3 && d < 21) return 'th'
   switch (d % 10) {
-    case 1:  return 'st'
-    case 2:  return 'nd'
-    case 3:  return 'rd'
+    case 1: return 'st'
+    case 2: return 'nd'
+    case 3: return 'rd'
     default: return 'th'
   }
 }
