c80db3092a1d611dbe8c60a9dc7e6b1a368acd94
Richard Tribe <Richard.Tribe@bbc.co.uk>
2024-09-17 12:35:41 +0100

    Add replacement for 'recursive-readdir' that just uses node's fs

--- src/generate-transform-input.js
+++ src/generate-transform-input.js
@@ -1,18 +1,57 @@
-const readdir = require('recursive-readdir')
+// const { readdir } = require('node:fs/promises')
 const fs = require('fs')
 const path = require('path')
 
 const generateTransformInput = (dir) => {
   dir = path.format(path.parse(dir))
 
-  return readdir(dir).then(files => {
-    return files.map(file => {
-      return {
-        relativePath: file.toString().replace(`${dir}/`, ''),
-        raw: fs.readFileSync(file)
+  const list = []
+  try {
+    const files = fs.readdirSync(dir, { recursive: true, withFileTypes: true })
+    // recursive option available from node 18+
+    // when options.withFileTypes set to true, the returned array will contain <fs.Dirent> objects.
+    for (const dirent of files) {
+      console.log('------')
+      console.log(`dirent: ${JSON.stringify(dirent, null, 2)}`)
+
+      // Node API for Dirent is unstable
+      const dirPath = dirent.path || dirent.parentPath // path is DEPRECATED! But parentPath does not work in 18.17
+
+      const fullPath = path.join(dirPath, dirent.name)
+      console.log('fullPath = ', fullPath)
+
+      if (dirent.isDirectory()) {
+        console.log('directory... continue')
+        continue
       }
-    })
-  })
+
+      if (dirent.isFile()) {
+        list.push(makeInputObject(fullPath, dir))
+        continue
+      }
+
+      if (dirent.isSymbolicLink) {
+        if (fs.existsSync(fullPath)) { // fs.exists() is deprecated, but fs.existsSync() is not.
+          console.log('Good symlink')
+          list.push(makeInputObject(fullPath, dir)) // symlinks become copies
+        } else {
+          console.log(`Broken symlink at: ${fullPath}`)
+        }
+        continue
+      }
+    }
+  } catch (err) {
+    console.error('Error in generateTransformInput', err)
+  }
+
+  return list
+}
+
+const makeInputObject = (fullPath, rootPath) => {
+  return {
+    relativePath: fullPath.replace(`${rootPath}/`, ''),
+    raw: fs.readFileSync(fullPath)
+  }
 }
 
 module.exports = generateTransformInput
