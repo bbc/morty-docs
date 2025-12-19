f0a2034f6c5ab7468e650d98ecdf0b660abef620
andysteadbbc <andy.stead@bbc.co.uk>
2025-12-19 13:21:10 +0000

    Replace showdown with marked for MD to HTML conversion

--- src/markdown-to-html-parser.js
+++ src/markdown-to-html-parser.js
@@ -1,38 +1,5 @@
-const showdown = require('showdown')
-
-// there may be an option to enable this, but since we haven't found it here is a reg-ex
-// to convert links within a file from *.md to *.html
-const convertMdLinksToHtmlLinks = {
-  type: 'output',
-  regex: /<a href="([^:\n]*?).md">/g,
-  // exclude colon, so external links aren't converted
-  replace: '<a href="$1.html">'
-}
-
-// Replace hash links with .html e.g. page.md#Title becomes page.html#Title.
-const convertMdHashLinksToHtmlLinks = {
-  type: 'output',
-  regex: /<a href="([^:\n]*?).md#(.*?)">/g,
-  // exclude colon, so external links aren't converted
-  replace: '<a href="$1.html#$2">'
-}
-
-const headingExtension = {
-  type: 'output',
-  regex: /<(h[123456]) id="([^"]+)">(.*)<\/\1>/g,
-  replace: '<$1 id="$2"><a href="#$2">$3</a></$1>'
-}
-
-const classMap = {
-  img: 'img-responsive',
-  table: 'table'
-}
-
-const bindings = Object.keys(classMap).map((key) => ({
-  type: 'output',
-  regex: new RegExp(`<${key}(.*)>`, 'g'),
-  replace: `<${key} class="${classMap[key]}" $1>`
-}))
+// const showdown = require('showdown')
+const { marked } = require('marked')
 
 const normaliseBasePath = (basePath) => {
   const pathElements = (basePath || '').split('/')
@@ -45,37 +12,61 @@ const normaliseBasePath = (basePath) => {
   return '/' + pathElements.join('/')
 }
 
-const createParser = (options) => {
-  const basePath = normaliseBasePath(options.basePath)
-  const addBasePathToRootLinks = {
-    type: 'output',
-    regex: /<a href="\/([^:\n]*)">/g, // exclude colon, so external links aren't converted
-    replace: `<a href="${basePath}/$1">`
-  }
-
-  const addBasePathToLinkHrefs = {
-    type: 'output',
-    regex: /<link(.+)href="\/([^:\n]*)"(.*)\/>/g, // exclude colon, so external links aren't converted
-    replace: `<link$1href="${basePath}/$2"$3/>`
-  }
-
-  const parser = new showdown.Converter({
-    extensions: [
-      convertMdLinksToHtmlLinks,
-      convertMdHashLinksToHtmlLinks,
-      addBasePathToRootLinks,
-      addBasePathToLinkHrefs,
-      headingExtension,
-      ...bindings
-    ]
-  })
-  parser.setFlavor('github')
-  return parser
-}
-
 const parseToHTML = (markdown, options = {}) => {
-  const parser = createParser(options)
-  return parser.makeHtml(markdown)
+  // const parser = createParser(options)
+  // return parser.makeHtml(markdown)
+  const basePath = normaliseBasePath(options.basePath)
+
+  function flattenHeading(text) {
+    // To match showdown behaviour
+    return text
+      .replace(/<(\w+)([^>]*)>/g, (match, tag, attrs) => {
+        // Extract id attribute if present
+        const idMatch = attrs.match(/id="([^"]+)"/)
+        return tag + (idMatch ? '-id' + idMatch[1] : '')
+      })
+      .replace(/<\/(\w+)>/g, '$1') // Replace closing tags with tag name
+      .replace(/[^a-zA-Z0-9 \-"=]+/g, '') // Remove special chars like ?!@ etc...
+      .replace(/[^\w-]+/g, '-') // Replace non-word chars with dashes
+      .replace(/^-+|-+$/g, '') // Trim leading/trailing dashes
+      .toLowerCase()
+  }
+
+  const renderer = {
+    heading({ tokens, depth }) {
+      const text = this.parser.parseInline(tokens)
+      const escapedText = flattenHeading(text.toLowerCase())
+
+      return `<h${depth} id="${escapedText}">${text}</h${depth}>`
+    },
+    blockquote({ tokens }) {
+      const textString = this.parser.parseInline(tokens[0].tokens)
+      const alertMatch = textString.match(/\[!(NOTE|TIP|WARNING|IMPORTANT|CAUTION)\]/i)
+
+      if (!alertMatch) return `<blockquote>${textString}</blockquote>`
+
+      const type = alertMatch[1].toLowerCase()
+      let content = textString.replace(/\[![A-Z]+\]/i, '')
+      content = content.replace(/^<br>/, '')
+      const title = type.charAt(0).toUpperCase() + type.slice(1).toLowerCase()
+      return `<blockquote class="markdown-alert markdown-alert-${type}"><p><strong>${title}</strong></p><p>${content}</p></blockquote>`
+    }
+  }
+
+  marked.use({ renderer, breaks: true })
+  let html = marked.parse(markdown).trim()
+
+  // Custom replacements
+  html = html.replace(/<a href="([^:\n]*?).md">/g, '<a href="$1.html">') // convertMdLinksToHtmlLinks
+  html = html.replace(/<a href="([^:\n]*?).md#(.*?)">/g, '<a href="$1.html#$2">') // convertMdHashLinksToHtmlLinks
+  html = html.replace(/<(h[123456]) id="([^"]+)">(.*)<\/\1>/g, '<$1 id="$2"><a href="#$2">$3</a></$1>') // headingExtension
+  html = html.replace(/<table(.*)>/g, '<table class="table" $1>') // add class to table tags
+  html = html.replace(/<img(.*)>/g, '<img class="img-responsive" $1 />') // add class to image tags
+  html = html.replace(/<li>(<input.*)<\/li>/g, '<li class="task-list-item">$1</li>') // add class for list items (must have input at beginning)
+  html = html.replace(/<a href="\/([^:\n]*)">/g, `<a href="${basePath}/$1">`) // addBasePathToRootLinks
+  html = html.replace(/<link(.+)href="\/([^:\n]*)"(.*)\/>/g, `<link$1href="${basePath}/$2"$3/>`) // addBasePathToLinkHrefs
+
+  return html
 }
 
 module.exports = parseToHTML
